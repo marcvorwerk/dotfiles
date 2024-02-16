18c8ac7d73f355eb4d8aeeb52222828d2eb314b6
Marc Vorwerk <marc+github@marc-vorwerk.de>
2024-02-16 07:47:56 +0100

    Switch ls hyperlink to auto

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -37,8 +37,9 @@ alias chgrp='chgrp --preserve-root'
 ############
 
 alias mkdir="mkdir -v "
-alias ll="ls --hyperlink --color=auto -lahv"
+alias ll="ls --hyperlink=auto --color=auto -lahv"
 alias eza="eza -la --hyperlink"
+alias eza-tree="eza --tree --color=always --icons=always --git-ignore --no-quotes --hyperlink'"
 alias bc="bc -l"
 alias tree='tree -a -I .git'
 alias cd-git-root="cd \$(git rev-parse --show-toplevel)"
