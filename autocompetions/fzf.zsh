fb64fcc7de68c1e0b4d180bef191bd64ce23f6f6
Marc Vorwerk <marc+github@marc-vorwerk.de>
2024-07-12 23:06:45 +0200

    Update fzf autocomletion

--- autocompetions/fzf.zsh
+++ autocompetions/fzf.zsh
@@ -1,13 +1,7 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *"$HOME/GIT/GITHUB/fzf/bin"* ]]; then
-  export PATH="${PATH:+${PATH}:}$HOME/GIT/GITHUB/MY/fzf/bin"
+if [[ ! "$PATH" == *"$HOME/GIT/GITHUB/MY/fzf/bin"* ]]; then
+  PATH="${PATH:+${PATH}:}$HOME/GIT/GITHUB/MY/fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "$HOME/GIT/GITHUB/MY/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "$HOME/GIT/GITHUB/MY/fzf/shell/key-bindings.zsh"
+source <(fzf --zsh)
